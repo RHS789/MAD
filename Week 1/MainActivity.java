d511bef81e099351d88a0a3de3db69b89b781433
RHS789 <23911a1242raghu@gmail.com>
2026-08-25 11:40:00 +0530

    Create MainActivity.java

---
+++ Week 1/MainActivity.java
@@ -0,0 +1,10 @@
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+
+public class MainActivity extends Activity{
+ public void onCreate(Bundle b){
+  super.onCreate(b);
+  setContentView(R.layout.activity_main);
+ }
+}
